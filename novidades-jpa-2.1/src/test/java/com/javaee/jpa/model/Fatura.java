57f034b9932e4b33cf017c8d2cc3c65c7d676331
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2020-02-11 06:45:40 -0300

    Implementando as funcionalidades do jpa 2.1

---
+++ novidades-jpa-2.1/src/test/java/com/javaee/jpa/model/Fatura.java
@@ -0,0 +1,79 @@
+package com.javaee.jpa.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_fatura")
+public class Fatura implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long codigo;
+	private BigDecimal valor;
+	private LocalDate vencimento;
+	private boolean vencida;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public LocalDate getVencimento() {
+		return vencimento;
+	}
+	public void setVencimento(LocalDate vencimento) {
+		this.vencimento = vencimento;
+	}
+	
+	public boolean isVencida() {
+		return vencida;
+	}
+	public void setVencida(boolean vencida) {
+		this.vencida = vencida;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Fatura other = (Fatura) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+}
